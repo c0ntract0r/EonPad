58702551a2878aed03d34d243f5b5ece10f96e9c
c0ntract0r <83899705+c0ntract0r@users.noreply.github.com>
2025-02-13 01:54:50 +0400

    connect to database

--- app.js
+++ app.js
@@ -1,12 +1,21 @@
-const express = require('express');
-const app = express();
-require('dotenv').config()
-
+const express = require('express')
+const app = express()
+require('dotenv').config();
+const connectDB = require('./config/connectDB')
 
 const PORT = process.env.PORT || 3001
+console.log(PORT);
+
+console.log(process.env.MONGO_URI);
 
 const SERVER_START = async () => {
-    app.listen(PORT, () => { console.log(`Server is listening on port 3000...`) });
+    try
+    {
+        await connectDB(process.env.MONGO_URI);
+        app.listen(PORT, () => { console.log(`Successfully connected to DB!\nServer is listening on port 3000...`) });
+    } catch (err) {
+        console.log(err);
+    }
 }
 
 SERVER_START();
