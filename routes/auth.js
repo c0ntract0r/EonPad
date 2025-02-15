afec760fcf28323756e29f5f868b949333503f62
c0ntract0r <83899705+c0ntract0r@users.noreply.github.com>
2025-02-16 02:05:26 +0400

    Assigned correct function to login route

--- routes/auth.js
+++ routes/auth.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const loginRouter = express.Router();
+const loginHandler  = require('../controllers/auth/login');
 
 loginRouter.route('/')
     .get((req, res) => { res.send('Testing login GET...') })
-    .post((req, res) => { res.send('Testing login POST...') });
+    .post(loginHandler.loginHandler);
 
 module.exports = loginRouter;
