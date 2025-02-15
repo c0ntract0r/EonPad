e4793577bafb2f5323dbae985456434241da74cf
c0ntract0r <83899705+c0ntract0r@users.noreply.github.com>
2025-02-15 22:38:07 +0400

    Added routes

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 3001;
 app.use(express.json());
 
 
-app.use('/login', require('./routes/login'));
+app.use('/login', require('./routes/auth'));
 app.use('/register', require('./routes/register'));
 app.use('/notes', require('./routes/notes'));
 
