38ff6a4e30621724679019b9f4e65b273add2a43
c0ntract0r <83899705+c0ntract0r@users.noreply.github.com>
2025-02-16 15:13:41 +0400

    Added JWT Verification

--- middlewares/jwtVerification.js
+++ middlewares/jwtVerification.js
@@ -1,1 +1,20 @@
 const jwt = require('jsonwebtoken');
+require('dotenv').config();
+const { StatusCodes } = require('http-status-codes');
+
+const verifyJWT = async (req, res, next) => {
+    const authHeader = req.headers.authorization;
+    if (!authHeader || !authHeader.startsWith('Bearer')) {
+        return res.status(StatusCodes.NOT_FOUND).json({ msg: 'No auth header!' });
+    }
+    const token = authHeader.split(' ')[1];
+    try {
+        const payload = jwt.verify(token, process.env.JWT_SECRET);
+        req.user = { user: payload.userID, name: payload.username };
+        next();
+    } catch(err) {
+        return res.status(StatusCodes.FORBIDDEN).json({ msg: 'Invalid token!' });
+    }
+}
+
+module.exports = verifyJWT;
