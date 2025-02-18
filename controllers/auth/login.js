0b8cd99e3f3595f23d4c07c312ef7caa2b404b2b
c0ntract0r <83899705+c0ntract0r@users.noreply.github.com>
2025-02-19 01:12:02 +0400

    Changed some logic again

--- controllers/auth/login.js
+++ controllers/auth/login.js
@@ -1,6 +1,7 @@
 const Users = require('../../models/users');
 const { StatusCodes } = require('http-status-codes');
-
+const genToken = require('../../utils/tokenCreator');
+require('dotenv').config();
 
 const loginHandler = async (req, res) => {
     const { username, password } = req.body;
@@ -14,9 +15,25 @@ const loginHandler = async (req, res) => {
         // right, this is for debugging for now
         return res.status(StatusCodes.UNAUTHORIZED).json({ msg: 'Incorrect password!' });
     }
-    const token = user.createJWT();
+    console.log(user);
+    const accessToken = genToken(user.username, 'a');
+    const refreshToken = genToken(user.username, 'r');
+    const expirationDate = Date.now() + 150000;
+
+    console.log(expirationDate);
+    await Users.updateOne(
+        {_id: user._id},
+        {
+            $push: {
+                refreshTokenDocs: {
+                    token: refreshToken,
+                    expirationDate: new Date(expirationDate)
+                }
+            }
+        }
+    );
     return res.status(StatusCodes.OK).json({ msg: 'Authentication successful', usr: {
-        user_name: user.username, user_token: token}})
+        user_name: user.username, user_token: {accessToken, refreshToken}}})
 }
 
 
