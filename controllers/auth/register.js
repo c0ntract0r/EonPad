c82e06c4172d237fff18d1974a03dc68dd634726
c0ntract0r <83899705+c0ntract0r@users.noreply.github.com>
2025-02-21 00:48:19 +0400

    Register route rewritten

--- controllers/auth/register.js
+++ controllers/auth/register.js
@@ -1,11 +1,26 @@
 const Users = require('../../models/users');
 const { StatusCodes } = require('http-status-codes');
+const bcrypt = require('bcryptjs');
 
 const RegistrationHandler = async (req, res) => {
+    const { username, password, first_name, last_name } = req.body;
     console.log(req.body);
-    // this is called spread syntax in JS
-    const user = Users.create({...req.body});
-    res.status(StatusCodes.CREATED).json({ user });
+    if (!username || !password) return res.status(400).json({ 'message': 'Username and password are required!' });
+    try {
+        const hashedPwd=  await bcrypt.hash(password, 10);
+        const newUser = new Users ({
+            first_name,
+            last_name,
+            username,
+            password: hashedPwd,
+        });
+        const result = await newUser.save();
+        console.log(result);
+        res.status(201).json({ 'success': `New user ${username} successfully created!` });
+    } catch (err) {
+        console.log(err);
+        return res.sendStatus(500);
+    }
 }
 
 module.exports = { RegistrationHandler };
