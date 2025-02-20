339dd69117c135dc77a6c015a5c051b38ffa0054
c0ntract0r <83899705+c0ntract0r@users.noreply.github.com>
2025-02-21 00:46:36 +0400

    Removed functions from Models, placing them in controllers

--- models/users.js
+++ models/users.js
@@ -24,14 +24,4 @@ const UserSchema = new mongoose.Schema ({
     folders: [folderSchema],
 });
 
-UserSchema.pre('save', async function (next) {
-    const salt = await bcrypt.genSalt(10);
-    this.password = await bcrypt.hash(this.password, salt);
-    next();
-})
-
-UserSchema.methods.comparePassword = async function(candidatePassword){
-    return await bcrypt.compare(candidatePassword, this.password);
-}
-
 module.exports = mongoose.model('Users', UserSchema);
