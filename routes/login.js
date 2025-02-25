6e8830651c79256e5df51ea729b8a8d2436b63a2
c0ntract0r <83899705+c0ntract0r@users.noreply.github.com>
2025-02-26 01:16:13 +0400

    Deleted unneccesary files

--- routes/login.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const loginRouter = express.Router();
-const loginHandler  = require('../controllers/auth/login');
-
-loginRouter.route('/')
-    .get((req, res) => { res.send('Testing login GET...') })
-    .post(loginHandler.loginHandler);
-
-module.exports = loginRouter;
--- routes/refresh.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const refreshRouter = express.Router();
-const refreshTokenHandler = require('../controllers/auth/refreshTokenController');
-
-refreshRouter.route('/')
-    .get(refreshTokenHandler.refreshTokenHandler)
-
-module.exports = refreshRouter;
