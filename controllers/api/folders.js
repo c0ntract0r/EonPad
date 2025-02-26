db2b6c97c9d5f06928bc64bf3761df48de20404c
c0ntract0r <83899705+c0ntract0r@users.noreply.github.com>
2025-02-27 00:46:13 +0400

    Made folders to work to the minimum state

--- controllers/api/folders.js
+++ controllers/api/folders.js
@@ -6,11 +6,12 @@ const getAllFolders = async (req, res) => {
 
 const createFolder = async (req, res) => {
     const { folderName } = req.body;
-    const foundUser = req.user.name;
-    const user = await Users.findOne({ username: foundUser });
+    const foundUser = req.user.user_id;
+    console.log(req.user);
+    const user = await Users.findById(foundUser);
     if (!user) { res.json({ 'msg': 'No user was found!' }) };
     if (!folderName) return res.sendStatus(404).json({ 'msg': 'Sorry,no folder name found.' });
-    user.folders.push({ folderName });
+    user.folders.push({ name: folderName });
 
     await user.save();
     return res.sendStatus(201);
