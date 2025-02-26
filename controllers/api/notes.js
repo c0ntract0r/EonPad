ef0725951119c0c70c3fab5827c93fb1d38531ef
c0ntract0r <83899705+c0ntract0r@users.noreply.github.com>
2025-02-27 00:46:57 +0400

    Made notes to work to the minimum state

--- controllers/api/notes.js
+++ controllers/api/notes.js
@@ -1,7 +1,27 @@
-const mongoose = require('mongoose');
+const Notes = require('../../models/notes');
+const Users = require('../../models/users');
 
-const NoteHandlerTest = async (req, res) => {
-    console.log('Testing some note router...')
-};
+const createNewNote = async (req, res) => {
+    const { noteTitle, noteBody } = req.body;
+    const foundUser = req.user.user_id;
+    if (!noteTitle || !noteBody) res.json({msg: 'OOPSIE! YOU MADE A FUCKIE WUCKIE!'});
+    try {
+        const user = Users.findById(foundUser);
+        if (!user) { res.json({ 'msg': 'No user was found!' }) };
+        const note = new Notes({
+            title: noteTitle,
+            body: noteBody,
+            user: foundUser
+        });
+        await note.save();
+        res.status(201).json({ message: 'Note created!', note });
+    } catch (err) {
+        console.log(err);
+    }
+}
 
-module.exports = { NoteHandlerTest };
+const getAllNotes = async (req, res) => {
+    res.json({msg: 'WHY?'});
+}
+
+module.exports = { createNewNote, getAllNotes };
