ee505b18fc23b32e80cfdf72bd507253209e15f2
c0ntract0r <83899705+c0ntract0r@users.noreply.github.com>
2025-04-13 11:46:08 +0400

    Delete models directory
    Inside server

--- models/tags.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const TagSchema = new mongoose.Schema({
-    userName: { type: mongoose.Schema.Types.ObjectId, ref: "Users", required: true },
-    tagName: { type: String, required: true },
-    tagColor: { type: String, default: "#000000" }
-});
-
-module.exports = mongoose.model('Tags', TagSchema);
