335b9e68087b080b7121d45b4ff40e0e80023723
c0ntract0r <83899705+c0ntract0r@users.noreply.github.com>
2025-04-13 11:45:53 +0400

    Delete routes directory
    Exists already

--- routes/auth/logout.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const logoutHandler  = require('../../controllers/auth/logoutController');
-const logoutRouter = express.Router();
-
-logoutRouter.route('/')
-    .get(logoutHandler.handleLogout);
-
-module.exports = logoutRouter;
--- routes/auth/register.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const registerRouter = express.Router();
-const registrationHandler = require('../../controllers/auth/register')
-
-registerRouter.route('/')
-    .get((req, res) => { res.send('Testing register GET route...') })
-    .post(registrationHandler.RegistrationHandler);
-
-module.exports = registerRouter;
