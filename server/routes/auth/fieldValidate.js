d5204c4ba2635cd6bbad152d210a67931d6c16e7
c0ntract0r <83899705+c0ntract0r@users.noreply.github.com>
2025-04-20 12:44:33 +0400

    Delete server/routes/auth/fieldValidate.js
    This file is not required

--- server/routes/auth/fieldValidate.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const regValidationHandler = express.Router();
-const ValidationHandler = require('../../controllers/auth/register');
-
-regValidationHandler.route('/')
-    .post(ValidationHandler.registrationValidationHandler);
-
-module.exports = regValidationHandler;
