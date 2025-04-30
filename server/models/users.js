0649c269ac7ee1ccd0feddaac71abf63fff37890
c0ntract0r <83899705+c0ntract0r@users.noreply.github.com>
2025-04-30 11:03:32 +0400

    Remove unnecessary code from user schemas

--- server/models/users.js
+++ server/models/users.js
@@ -8,9 +8,8 @@ const UserSchema = new mongoose.Schema ({
     password: { type:String, required: true},
     refreshToken: [{ type: String, default: 'empty' }],
     createdAt: { type:Date, default: Date.now() },
-    // The notes belonging to the user
-    notes: { type: mongoose.Schema.Types.ObjectId, ref: 'Notes' },
-    folders: { type: mongoose.Schema.Types.ObjectId, ref: 'Folders' }
+    // notes: { type: mongoose.Schema.Types.ObjectId, ref: 'Notes' },
+    // folders: { type: mongoose.Schema.Types.ObjectId, ref: 'Folders' }
 }, { timestamps: true });
 
 module.exports = mongoose.model('Users', UserSchema);
