f513012f2531b5ac8a56598c431dd441afa40382
c0ntract0r <83899705+c0ntract0r@users.noreply.github.com>
2025-05-05 23:19:00 +0400

    Create login page. Bugs still exist

--- client/src/Pages/Login-Page/LoginPage.jsx
+++ client/src/Pages/Login-Page/LoginPage.jsx
@@ -1,14 +1,15 @@
 import { useState } from "react";
 import { InputFields } from "../../Components";
 import './LoginPage.css'
+import { useMutation } from "@tanstack/react-query"
+import { login } from "../../utils/api";
+import { useNavigate } from "react-router";
 
-// should be placed somewhere else, clutters like hell
-const loginUrl = "http://localhost:3000/login"
 
 // Login-specific input fields
 const inputValues = [
-  { id: 1, username: "username", type: "text", label: "Username" },
-  { id: 2, password: "password", type: "password", label: "Password" }
+  { id: 1, name: "username", type: "text", label: "Username" },
+  { id: 2, name: "password", type: "password", label: "Password" }
 ];
 
 const LoginPage = () => {
@@ -17,9 +18,21 @@ const LoginPage = () => {
     username: "",
     password: ""
   });
+  const navigate = useNavigate();
+
+  // needs to be call to backend
+  const { mutate: signIn, isPending, isError } = useMutation({
+    mutationFn: login,
+    onSuccess: () => {
+      navigate('/', {
+        // Don't go back to login page
+        replace: true
+      })
+    }
+  });
 
   const handleChange = async (event) => {
-    setValues({...values, [event.target.name] : event.target.value})
+    setValues({...values, [event.target.name] : event.target.value});
   }
 
   const handleSubmit = (event) => {
@@ -30,12 +43,16 @@ const LoginPage = () => {
     <main>
       <h2 className="login-title">Login</h2>
       <div className="form-container">
+        {
+          isError && (<h1>Invalid username or password!</h1>)
+        }
         <form onSubmit={handleSubmit}>
           {inputValues.map((input) => (
           <InputFields key={input.id} onChange={handleChange} {...input} value={values[input.name]} />
           ))}
-          <button type="submit" className="submit-btn">Go to notes</button>
+          <button onKeyDown={(e) => e.key === "Enter" && signIn({ username: values.username, password: values.password })} onClick={ () => signIn({ username: values.username, password: values.password }) } disabled={ !values.username ||  values.password.length < 8 } type="submit" className="submit-btn">Go to notes</button>
         </form>
+        <a className="anchor" href="/register">Don't have an account? Sign up.</a>
       </div>
     </main>
   )
