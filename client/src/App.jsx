659052ae0a235d62d16c61ccd2e93f294734f7bd
c0ntract0r <83899705+c0ntract0r@users.noreply.github.com>
2025-06-10 21:14:00 +0400

    Main files

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,13 @@
 import { RouterProvider, createBrowserRouter } from "react-router";
-import { Register, Landing, Error, Login, About, Layout } from './Pages';
+import { Register, Landing, Error, Login, About, Layout, Dashboard } from './Pages';
+
+// loaders
+import { loader as dashboardLoader } from './Pages/Dashboard';
 
 // Actions
 import { action as RegisterAction } from "./Pages/Register";
 import { action as loginAction } from "./Pages/Login";
+import { store } from './store';
 
 const router = createBrowserRouter([
   {
@@ -26,13 +30,20 @@ const router = createBrowserRouter([
   {
     path: '/login',
     element: <Login />,
-    errorElement: <Error />
+    errorElement: <Error />,
+    action: loginAction(store),
   },
   {
     path: '/register',
     element: <Register />,
     errorElement: <Error />,
     action: RegisterAction
+  },
+  {
+    path: '/dashboard',
+    element: <Dashboard />,
+    errorElement: <Error />,
+    loader: dashboardLoader(store),
   }
 ])
 
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -13,33 +13,4 @@ createRoot(document.getElementById('root')).render(
   </Provider>
 )
 
-/* 
 
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
-import queryClient from './services/queryClient';
-import { QueryClientProvider } from "@tanstack/react-query"
-
-
-
-// define all front routes for the page
-const router = createBrowserRouter([{
-  path: '/register',
-  element: <RegisterPage />,
-  },
-  {
-    path: '/login',
-    element: <LoginPage />
-  },
-])
-
-
-createRoot(document.getElementById('root')).render(
-    <StrictMode>
-      <QueryClientProvider client={queryClient}>
-        <RouterProvider router={router} />
-        <ReactQueryDevtools position="bottom-right" initialIsOpen={false} />
-      </QueryClientProvider>
-    </StrictMode>
-)
-
-*/
